2b5de709bcd67ece15932e624f082192beaad1ab
Andrii Reverchuk <andrejrevercuk@gmail.com>
2021-07-03 00:14:21 +0300

    add handle_container

--- src/MVC/Model.js
+++ src/MVC/Model.js
@@ -1,31 +1,28 @@
-export class Model{
-   liquidProducts = [{
-      name:"Кола",
-      volume:20,
-      type:"liquid"
-   },
-   {
-      name:"Квас",
-      volume:99,
-      type:"liquid"
-   }];
-   buldProducts = [{
-      name:"Сахар",
-      volume:28,
-      type:"bulk"
-   },
-   {
-      name:"Рис",
-      volume:45,
-      type:"bulk"
-   },
+export class Model {
+   products = [
+      {
+         name: "Кола",
+         volume: 20,
+         type: "Liquid Products",
+      },
+      {
+         name: "Квас",
+         volume: 99,
+         type: "Liquid Products",
+      },
+      {
+         name: "Сахар",
+         volume: 28,
+         type: "Buld Products",
+      },
+      {
+         name: "Рис",
+         volume: 45,
+         type: "Buld Products",
+      },
    ];
-   constructor(){
-   }
-   getLiquidProducts(){
-      return this.liquidProducts;
-   }
-   getBulkProducts(){
-      return this.buldProducts;
+   constructor() {}
+   getProducts() {
+      return this.products;
    }
 }
