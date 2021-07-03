fc4d4d59112c5a159924f982f0f5367ae4e168d8
Andrii Reverchuk <andrejrevercuk@gmail.com>
2021-07-03 20:37:57 +0300

    done

--- src/MVC/Controller.js
+++ src/MVC/Controller.js
@@ -1,4 +1,4 @@
-import { Container } from "./Container";
+import { Container } from './Container';
 export class Controller {
    constructor(model, view) {
       this.model = model;
@@ -7,19 +7,74 @@ export class Controller {
       this.handleAddContainer();
    }
    handleAddContainer() {
-      const btn = this.view.getElement("#add-btn");
-      btn.addEventListener("click", () => {
-         const nameForAdd = this.view.getElement("#nameForAdd").value;
-         const selectForAdd = this.view.getElement("#selectForAdd").value;
-         const amountForAdd = this.view.getElement("#amountForAdd").value;
-         this.model.getProducts().push({
-            name: nameForAdd,
-            volume: amountForAdd,
-            type: selectForAdd,
+      const btnAdd = this.view.getElement('#add-btn');
+      const btnDel = this.view.getElement('#btn-delete');
+      btnAdd.addEventListener('click', () => {
+         const nameForAdd = this.view.getElement('#nameForAdd').value;
+         const selectForAdd = this.view.getElement('#selectForAdd').value;
+         const amountForAdd = this.view.getElement('#amountForAdd').value;
+         let index;
+         this.model.getProducts().forEach((element, i) => {
+            if (nameForAdd.toLowerCase() === element.name.toLowerCase()) {
+               index = i;
+            }
+         });
+         if (index) {
+            let volume = this.model.getProducts()[index].volume + +amountForAdd;
+            if (volume <= 100) {
+               this.model.getProducts()[index].volume = volume;
+               alert(`Контейнер ${nameForAdd} существует доавляем в него`);
+
+               console.log(this.model.getProducts()[index].volume);
+            } else {
+               alert(`Контейнер ${nameForAdd} переполнен  создаю еще один`);
+               this.model.getProducts()[index].volume = 100;
+               this.model.getProducts().push({
+                  name: nameForAdd,
+                  volume: volume - this.model.getProducts()[index].volume,
+                  type: selectForAdd,
+               });
+            }
+         } else {
+            let result = window.confirm(
+               'Такого контейнера нет!!! Создать его?'
+            );
+            if (result) {
+               this.model.getProducts().push({
+                  name: nameForAdd,
+                  volume: amountForAdd,
+                  type: selectForAdd,
+               });
+            }
+         }
+         this.view.removeProducts();
+         this.renderItems(this.model.getProducts());
+      });
+      btnDel.addEventListener('click', () => {
+         const nameForDel = this.view.getElement('#inputForNameDelete').value;
+         const selectForDel = this.view.getElement('#selectForDelete').value;
+         const amountForDel = this.view.getElement('#amountForDelete').value;
+         let index;
+         this.model.getProducts().forEach((element, i) => {
+            if (nameForDel.toLowerCase() === element.name.toLowerCase()) {
+               index = i;
+               if (element.volume <= +amountForDel) {
+                  let result = window.confirm(
+                     'В контейнере ничего не осталось!!! Удалить его?'
+                  );
+                  if (result) {
+                     this.model.getProducts().splice(i, 1);
+                     console.log(this.model.getProducts());
+                  } else {
+                     element.volume = 0;
+                  }
+               } else {
+                  element.volume -= amountForDel;
+               }
+            }
          });
          this.view.removeProducts();
          this.renderItems(this.model.getProducts());
-         console.log(nameForAdd, selectForAdd, amountForAdd);
       });
    }
    renderItems(product) {
